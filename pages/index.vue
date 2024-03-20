e420f1e591a4b5c24e158f843d40e3309c70af6a
Bahador-Aryaeefar <bahadoraryaeefar@gmail.com>
2024-03-20 16:50:44 +0330

    scrolls

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
-    <div class="flex justify-between">
+    <div class="flex justify-between h-[calc(100dvh-7rem)] mt-[7rem]">
         <Editor></Editor>
-        <div class="grow">
+        <div class="grow overflow-auto">
             <Datapath class="mx-auto"></Datapath>
         </div>
     </div>
